7937781ce01138ed085931c38cc554ab403f0955
yumin <sym2596@knu.ac.kr>
2025-06-30 02:08:08 +0900

    feat: 진입점 추가

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    cout << "Hello File Explorer!" << endl;
 
+    for (int i = 1; i < argc; i++) {
+        string arg = argv[i];
+        if (arg == "--help") {
+            cout << "File Explorer CLI 도움말\n";
+            // TODO: 도움말 만들기
+            return 0;
+        }
+
+        if (arg == "--gui") {
+            cout << "GUI 모드는 추후 지원 예정입니다.\n";
+            return 0;
+        }
+    }
+
+    cout << "File Explorer 시작\n";
+    // TODO: 파일 탐색 기능 시작
     return 0;
 }
